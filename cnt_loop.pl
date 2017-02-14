2abf4b490f6a18cd7e73b7265e4061f5d4f3be4a
tauomicronmu <tauomicronmurs@gmail.com>
2017-02-14 11:24:11 +0000

    infinitely counting loop

--- cnt_loop.pl
+++ cnt_loop.pl
@@ -1,4 +1,6 @@
+cnt_loop() :- cnt_loop(0).
 cnt_loop(N) :- N >= 0,
-               write(N),
+               string_concat(N, "\n", X),
+               write(X),
                M is N + 1,
                cnt_loop(M).
