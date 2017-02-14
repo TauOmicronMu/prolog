5e142ba7e17c84c56fc37a1b2d7480e140cfd005
tauomicronmu <tauomicronmurs@gmail.com>
2017-02-14 11:56:26 +0000

    got towers of hanoi working - goddamn prolog does towers well o.O

--- towers.pl
+++ towers.pl
@@ -1,3 +1,12 @@
+move([D], Start, _, Goal) :- 
+    write("Move "),
+    write(D),
+    write(" from "),
+    write(Start),
+    write(" to "),
+    write("Goal"),
+    nl.
+
 move([B | R], Start, Swap, Goal) :-
     move(R, Start, Goal, Swap),
     move([B], Start, _, Goal),
